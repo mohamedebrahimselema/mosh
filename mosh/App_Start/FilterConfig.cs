1ec7939e595044bce8f3ddb5d48179feb7b10853
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-12-09 15:25:29 +0200

    Add project files.

---
+++ mosh/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace mosh
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ mosh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mosh.MvcApplication" Language="C#" %>
